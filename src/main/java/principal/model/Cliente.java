765c446849601a0b4d664f0d6f8d7988a37b2609
fuhr-br <andersonfuhr@yahoo.com.br>
2021-11-10 15:46:18 -0300

    Adicionada classe cliente

---
+++ src/main/java/principal/model/Cliente.java
@@ -0,0 +1,18 @@
+package principal.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Clientes")
+public class Cliente extends Pessoa{
+	
+	
+	public Cliente() {
+
+	}
+
+	
+	
+
+}
--- src/main/java/principal/model/Locatario.java
+++ src/main/java/principal/model/Locatario.java
@@ -10,12 +10,12 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "Locatarios")
 public class Locatario extends Pessoa {
-	
-	
+
 	@OneToMany
 	@JoinColumn(name = "Imoveis_ID")
 	private List<Imovel> Imovel;
-	
-	public Locatario() {}
+
+	public Locatario() {
+	}
 
 }
