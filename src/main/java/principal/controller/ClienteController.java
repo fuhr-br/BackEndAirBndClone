e2d5cc4a6b7218d7c137f6c67a64fea748a1db1c
fuhr-br <andersonfuhr@yahoo.com.br>
2021-11-10 17:52:45 -0300

    create(Class): ClienteController

--- src/main/java/principal/controller/ClienteController.java
+++ src/main/java/principal/controller/ClienteController.java
@@ -1,12 +1,16 @@
 package principal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import principal.services.ServiceCliente;
 
 @Controller
 @RequestMapping(value = "/cliente")
 public class ClienteController {
 	
-	
+	@Autowired
+    private ServiceCliente service;
 
 }
