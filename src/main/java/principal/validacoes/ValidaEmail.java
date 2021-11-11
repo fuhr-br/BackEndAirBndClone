35df782dbabf49165c6474a670210c54e197ad1c
fuhr-br <andersonfuhr@yahoo.com.br>
2021-11-11 20:52:55 -0300

    deploy heroku

--- src/main/java/principal/validacoes/ValidaEmail.java
+++ src/main/java/principal/validacoes/ValidaEmail.java
@@ -7,8 +7,8 @@ public class ValidaEmail {
 	
 	//interface funcionar com lambda para validar se é um email
 	public static Predicate<String> isEmail = email -> {
-		String validaemail = "^(.+)@(.+)$";
-		Pattern pattern = Pattern.compile(validaemail);
+		var validaemail = "^(.+)@(.+)$";
+		var pattern = Pattern.compile(validaemail);
 		return pattern.matcher(email).matches(); 
 	};
 	//interface funcional com lambda para verificar se está vazio
