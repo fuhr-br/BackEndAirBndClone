b02ef704d5b61c9650461a8f025d0caf64c7d5d6
fuhr-br <andersonfuhr@yahoo.com.br>
2021-11-14 14:59:31 -0300

    refatoração Arquivocontroller

--- src/main/java/principal/ArquivoController.java
+++ src/main/java/principal/ArquivoController.java
@@ -17,7 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/api")
 public class ArquivoController {
 
 	@Autowired
