81ef3f97d11a39dac21a8681b3a96f6fbc818e96
guismoreira <guilhermemoreira55@gmail.com>
2021-11-25 22:25:16 -0300

    Testar classe Usuario #37

--- src/test/java/principal/model/UsuarioTest.java
+++ src/test/java/principal/model/UsuarioTest.java
@@ -1,15 +1,41 @@
 package principal.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
 
 class UsuarioTest {
     @Test
 	void testGetNome() {
-		Usuario usuario = new Usuario("Nicolas Bitenca",18,"(51) 98454-5458","Jack@yahoo.com");
+		Usuario usuario = new Usuario("Nicolas Bitenca", 18,"(51) 98454-5458","Jack@yahoo.com");
 
 		assertEquals(usuario.getNome(), "Nicolas Bitenca");
+		assertFalse(usuario.getNome().equals("Avril"));
+	}
+
+	@Test
+	void testGetIdade() {
+		Usuario usuario = new Usuario("Nicolas Bitenca", 18,"(51) 98454-5458","Jack@yahoo.com");
+
+		assertEquals(usuario.getIdade(), 18);
+		assertFalse(usuario.getIdade().equals(30));
+	}
+
+	@Test
+	void testGetTelefone() {
+		Usuario usuario = new Usuario("Nicolas Bitenca", 18,"(51) 98454-5458","Jack@yahoo.com");
+
+		assertEquals(usuario.getTelefone(), "(51) 98454-5458");
+		assertFalse(usuario.getTelefone().equals("(51) 99999-9999"));
+	}
+
+	@Test
+	void testGetEmail() {
+		Usuario usuario = new Usuario("Nicolas Bitenca", 18,"(51) 98454-5458","Jack@yahoo.com");
+
+		assertEquals(usuario.getEmail(), "Jack@yahoo.com");
+		assertFalse(usuario.getEmail().equals("wrong@email.com"));
 
 	}
 }
